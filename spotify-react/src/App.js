43e28d1802d8205f32da9a71d1f0583d782e2f08
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-02 23:51:43 -0500

    WIP

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -7,6 +7,17 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if(token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token=")).split("=")[1]
+    }
+  })
+
   return (
     <div className="App">
       <header className="App-header">
