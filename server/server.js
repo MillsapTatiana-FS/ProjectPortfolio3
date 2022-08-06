2ba8e8b6c3af597a301a985c16eaacbaa8b76a54
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-06 00:03:21 -0500

    Working on AuthJS

--- server/server.js
+++ server/server.js
@@ -7,6 +7,30 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: 'd6f9ca3d9254465c9820c56c43246f1e',
+    clientSecret: 'a96119c60e8640ffb3d7a66d2e748a77',
+    refreshToken,
+  })
+
+  spotifyApi
+  .refreshAccessToken()
+  .then((data) => {
+    res.json({
+      accessToken: data.body.accessToken,
+      expiresIn: data.body.expiresIn
+    })
+    
+  })
+  .catch(() => {
+    res.sendStatus(400)
+    })
+  })
+
+
 app.post('/login', (req, res) => {
   const { code } = req.body.code;
   const spotifyApi = new SpotifyWebApi({
--- spotify-react/src/useAuth.js
+++ spotify-react/src/useAuth.js
@@ -8,11 +8,34 @@ export default function useAuth(code) {
   const [expiresIn, setExpiresIn] = useState()
 
     useEffect(() => {
-        axios.post('http://localhost:3001/login', {code,})
+        axios
+        .post("http://localhost:3001/login", {
+            code,
+        })
         .then(res => {
-            console.log(res.data)
-        }).catch(() => {
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({}, null, "/")
+        })
+        .catch(() => {
             window.location = '/'
         })
     }, [code])
+
+    useEffect(() => {
+        axios
+        .post("http://localhost:3001/refresh", {
+            refreshToken,
+        })
+        .then(res => {
+            setAccessToken(res.data.accessToken)
+            setExpiresIn(res.data.expiresIn)
+        })
+        .catch(() => {
+            window.location = '/'
+        })
+    }, [refreshToken, expiresIn])
+
+    return accessToken
 }
