0da9f701150207510403c680e71174cff10b893e
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-08 18:30:57 -0500

    Working on auth

--- spotify-react/src/Login.js
+++ spotify-react/src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=d6f9ca3d9254465c9820c56c43246f1e&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-private%20user-read-email%20user-library-modify%20user-library-read&show_dialog=true"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=d6f9ca3d9254465c9820c56c43246f1e&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20&show_dialog=true"
 
 export default function Login() {
   return (
