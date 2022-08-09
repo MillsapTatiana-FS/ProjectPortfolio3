b017a86f4936637148b8b38c3df8d42983c80776
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-09 02:03:24 -0500

    Working on OAuth

--- server/server.js
+++ server/server.js
@@ -1,58 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const SpotifyWebApi = require('spotify-web-api-node');
+const express = require("express") 
+require("dotenv" ) .config() ;
+const mongoose = require ("mongoose") ;
+const cors = require("cors") ;
+const app = express () 
 
-const app = express()
 app.use(cors())
-app.use(bodyParser.json())
 
-console.log("Hello")
+const PORT = process.env. PORT || 3000;
 
-app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'http://localhost:3000',
-    clientId: 'd6f9ca3d9254465c9820c56c43246f1e',
-    clientSecret: 'a96119c60e8640ffb3d7a66d2e748a77',
-    refreshToken,
-  })
-
-  spotifyApi
-  .refreshAccessToken()
-  .then((data) => {
-    res.json({
-      accessToken: data.body.accessToken,
-      expiresIn: data.body.expiresIn,
-    })
-    
-  })
-  .catch(() => {
-    res.sendStatus(400)
-    })
-  })
-
-
-app.post("/login", (req, res) => {
-  const code  = req.body.code
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'http://localhost:3000',
-    clientId: 'd6f9ca3d9254465c9820c56c43246f1e',
-    clientSecret: 'a96119c60e8640ffb3d7a66d2e748a77',
-  })
-
-  spotifyApi
-  .authorizationCodeGrant(code)
-  .then(data => {
-    res.json({
-      accessToken: data.body.access_token,
-      refreshToken: data.body.refresh_token,
-      expiresIn: data.body.expires_in,
-    })
-  })
-  .catch(err => {  
-    res.sendStatus(400)
-  })
+app. listen(PORT, () => {
+console. log (`Server running on ${PORT}.`) 
 })
-
-app.listen(3001)
--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,12 +1,31 @@
-import Login from './Login';
-import Dashboard from './Dashboard';
+import { useState, useEffect } from "react";
 
-const code = new URLSearchParams(window.location.search).get('code')
+const CLIENT_ID = "d6f9ca3d9254465c9820c56c43246f1e"
+const CLIENT_SECRET = "a96119c60e8640ffb3d7a66d2e748a77"
 
 function App() {
-  return code ? <Dashboard code={code} /> : <Login />
   
+  useEffect(() => {
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+  })
+  // const REDIRECT_URI = "http://localhost:3000"
+  // const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  // const RESPONSE_TYPE = "token"
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Spotify React</h1>
+      </header>
+    </div>
+  );
 }
-  
- 
+
 export default App;
