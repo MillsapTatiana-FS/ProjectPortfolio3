388c0c0d09f4c8c7fd514f65da7d48aeeb756f2d
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-09 14:21:49 -0500

    Working on OAuth login

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,23 +1,5 @@
-import { useState, useEffect } from "react";
-
-const CLIENT_ID = "d6f9ca3d9254465c9820c56c43246f1e"
-const CLIENT_SECRET = "a96119c60e8640ffb3d7a66d2e748a77"
 
 function App() {
-  
-  useEffect(() => {
-    var authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
-    }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-  })
-  // const REDIRECT_URI = "http://localhost:3000"
-  // const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  // const RESPONSE_TYPE = "token"
 
   return (
     <div className="App">
---
+++ spotify-react/src/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=d6f9ca3d9254465c9820c56c43246f1e&response_type=token&redirect_uri=http://localhost:3000&scope=streaming%20user-read-private%20user-read-email%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state&state=34fFs29kd09'
+
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
