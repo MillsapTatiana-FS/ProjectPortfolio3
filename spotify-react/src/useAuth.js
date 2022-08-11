747d351574f72c25d4b5fb9751936d557f80f623
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-10 20:04:14 -0500

    Working on refresh Token

--- spotify-react/src/useAuth.js
+++ spotify-react/src/useAuth.js
@@ -7,11 +7,35 @@ export default function useAuth(code) {
   const [refreshToken, setRefreshToken] = useState()
   const [expiresIn, setExpiresIn] = useState()
 
+  console.log(refreshToken)
+
     useEffect(() => {
         axios.post('http://localhost:3001/login', {
             code,
-        }).then(res => {
-            console.log(res.data)
+        })
+        .then(res => {
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({}, null, '/')
+        })
+        .catch(() => {
+            //window.location = '/'
         })
     }, [code])
+
+    useEffect(() => {
+        axios.post('http://localhost:3001/refresh', {
+            refreshToken,
+        })
+        .then(res => {
+            setAccessToken(res.data.accessToken)
+            setExpiresIn(res.data.expiresIn)
+        })
+        .catch(() => {
+            //window.location = '/'
+        })
+    }, [refreshToken, expiresIn])
+
+    return accessToken
 }
