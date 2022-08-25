4fac6be24545efe28cc3325ba09d6d54247572e9
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-24 22:40:03 -0500

    Working on backend

--- api/models/spotifytoken.js
+++ api/models/spotifytoken.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
 const spotifyTokenSchema = new mongoose.Schema({
-    access_token: { type: String, required: true },
-    expires_in: { type: Number, required: true},
-    refresh_token: { type: String, required: true },
-},
-    );
+    access_token: {
+        type: String, 
+        required: true 
+    },
+    expires_in: {
+        type: Number, 
+        required: true
+    },
+    refresh_token: {
+        type: String, 
+        required: true 
+    },
+});
 
 module.exports = mongoose.model('SpotifyToken', spotifyTokenSchema);
