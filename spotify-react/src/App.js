7de4aa38d52d9a54727191504326fe49286c3e4c
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-24 22:29:55 -0500

    Finishing backend

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -5,7 +5,7 @@ const token = new URLSearchParams(window.location.search).get('token')
 function App(){
   return (
     <div>
-       !token <Login /> : <Home />
+      <Login />
     </div>
   )
 }
