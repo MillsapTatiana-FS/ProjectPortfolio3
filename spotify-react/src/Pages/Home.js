1b1292b307683c96c4b43891f02ad82873e71ccc
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-25 01:34:59 -0500

    Working on backend

--- spotify-react/src/Pages/Home.js
+++ spotify-react/src/Pages/Home.js
@@ -1,46 +1,48 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SideNav from '../Components/SideNav/sideNav';
-import Favorites from './Favorites';
-import Feed from './Feed';
-import Library from '../Pages/Library/Library';
-import Player from './Player';
-import Trending from './Trending';
-import Login from './Login';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SideNav from "../Components/SideNav/sideNav";
+import Favorites from "./Favorites";
+import Feed from "./Feed";
+import Library from "../Pages/Library/Library";
+import Player from "./Player";
+import Trending from "./Trending";
+import Login from "./Login";
 
-const token = new URLSearchParams(window.location.search).get('token');
+const token = new URLSearchParams(window.location.search).get("token");
 
 function Home() {
   let ignore = false;
-   useEffect(()=>{
-     if(!ignore){
-         axios.post('http://localhost:3001/spotify/v1/auth', {
-         token
-         }).then(res =>{
-             console.log(res.data)
-         }).catch(()=>{
-             //window.location ='/'
-         })
-     }
-     return()=>{
-     ignore = true;
-     }
+  useEffect(() => {
+    if (!ignore) {
+      axios
+        .post("http://localhost:3001/spotify/v1/auth", {
+          token,
+        })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch(() => {
+          //window.location ='/'
+        });
+    }
+    return () => {
+      ignore = true;
+    };
+  }, [token]);
 
-   }, [token])
-
-   return (  
+  return (
     <Router>
-        <div style={styles.mainBody}>
-          <SideNav />
-            <Routes>
-              <Route path="/" element={<Library />} />
-              <Route path="/feed" element={<Feed />} />
-              <Route path="/trending" element={<Trending />} />
-              <Route path="/player" element={<Player />} />
-              <Route path="/favorites" element={<Favorites />} />
-            </Routes>
-        </div>
+      <div style={styles.mainBody}>
+        <SideNav />
+        <Routes>
+          <Route path="/" element={<Library />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
@@ -49,10 +51,10 @@ export default Home;
 
 const styles = {
   mainBody: {
-    height: '100vh',
-    width: '100vw',
-    backgroundColor: '#f7e2fb',
-    borderRadius: '30px',
-    display: 'flex',
+    height: "100vh",
+    width: "100vw",
+    backgroundColor: "#f7e2fb",
+    borderRadius: "30px",
+    display: "flex",
   },
-}
+};
