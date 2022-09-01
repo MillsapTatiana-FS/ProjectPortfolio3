f236d694d24cdd23ad1081c00fc1bc0f8746b205
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-08-31 20:51:58 -0500

    working on finial touches

--- spotify-react/src/Components/SideNav/sideNav.js
+++ spotify-react/src/Components/SideNav/sideNav.js
@@ -1,26 +1,27 @@
-import React, { useState, useEffect} from 'react';
-import { MdFavorite } from 'react-icons/md';
-import { FaGripfire, FaPlay } from 'react-icons/fa';
-import { FaSignOutAlt } from 'react-icons/fa';
-import { IoLibrary } from 'react-icons/io5';
-import { MdSpaceDashboard } from 'react-icons/md';
-import SideNavButton from './sideNavButton';
-import './sideNav.css';
+import React, { useEffect, useState } from "react";
+import { FaPlay } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+//import { MdSpaceDashboard } from 'react-icons/md';
+import { FaSignOutAlt } from "react-icons/fa";
+import SideNavButton from "./SideNavButton";
+import "./SideNav.css";
+import apiClient from "../../spotify";
 
-export default function Sidebar() {
+export default function SideNav() {
   const [image, setImage] = useState(
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU"
   );
-  
+  useEffect(() => {
+    apiClient.get("me").then((response) => {
+      setImage(response.data.images[0].url);
+    });
+  }, []);
+
   return (
     <div className="sidebar-container">
       <img src={image} className="profile-img" alt="profile" />
       <div>
-        <SideNavButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
-        <SideNavButton title="Trending" to="/trending" icon={<FaGripfire />} />
         <SideNavButton title="Player" to="/player" icon={<FaPlay />} />
-        <SideNavButton title="Favorites"to="/favorites"icon={<MdFavorite />}
-        />
         <SideNavButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
       <SideNavButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
